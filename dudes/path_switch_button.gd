12b0043dd185b40dec97f7e8e138285722acf7d3
Piet Goris <piet.goris@gmail.com>
2022-11-12 22:27:55 +0100

    Added space occupiers

--- dudes/path_switch_button.gd
+++ dudes/path_switch_button.gd
@@ -6,8 +6,7 @@ func _process(delta):
 	if(get_parent().is_path_active(active_path)):
 		texture = preload("res://assets/UI/arrow_active.png")
 	else:
-		pass
-		#texture = preload()
+		texture = preload("res://assets/UI/arrow_non_active.png")
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if(event.is_action_pressed("select_path")):
