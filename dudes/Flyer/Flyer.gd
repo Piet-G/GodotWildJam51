3a7e27babd1629a83c966b14feb774e1ce137f1d
stan depuydt <stanneke.depuydt@gmail.com>
2022-11-16 23:24:25 +0100

    added flyers

---
+++ dudes/Flyer/Flyer.gd
@@ -0,0 +1,68 @@
+extends PathFollow2D
+
+export var health = 1
+export var movement_speed = 50
+export var is_enemy = false
+export var castle_damage = 1
+
+export(SpriteFrames) var regular_sprites
+export(SpriteFrames) var enemy_sprites
+var targeted = false
+var sneaking = false
+var direction = Vector2.ZERO
+var target = Vector2(760,40)
+var rot = (-PI + 0.5)
+var active = false
+
+func _ready():
+	$AnimatedSprite.z_index = 100
+	$Sprite.z_index = 100
+	if(not is_enemy):
+		$AnimatedSprite.frames = regular_sprites
+		$AnimatedSprite.position.x = $AnimatedSprite.position.x - 5
+	else:
+		rot = + 0.7
+		target = Vector2(80,520)
+		$AnimatedSprite.frames = enemy_sprites
+		$AnimatedSprite.position.x = $AnimatedSprite.position.x - 10
+	
+func get_health():
+	return health
+
+func sneak():
+	$SneakTimer.start()
+	$AnimatedSprite.modulate = Color(0.2,0.2,0.2,0.4)
+	sneaking = true
+
+func stop_sneaking():
+	$SneakTimer.stop()
+	$AnimatedSprite.modulate = Color(1,1,1,1)
+	sneaking = false
+
+func damage(amount):
+	health -= amount
+	if(health <= 0):
+		queue_free()
+
+func is_active():
+	return active
+	
+func get_z_position():
+	return global_position.y - 20
+
+func _physics_process(delta):
+	if(not is_active()):
+		return
+	var direction = (target - global_position).normalized()
+	global_position = global_position + (direction*movement_speed*delta)
+	rotation = global_position.angle_to_point(target) + rot
+	
+	if(global_position.distance_to(target) < 50):
+			
+			CastleService.damage(castle_damage, not is_enemy)
+			free()
+			return
+
+
+func _on_SneakTimer_timeout():
+	stop_sneaking()
---
+++ towers/barracks/Airfield/Airfield.gd
@@ -0,0 +1,64 @@
+extends "res://towers/tower.gd"
+
+export(String, FILE, "*.tscn") var unit_scene
+export var max_dudes = 5
+
+var to_war_count = 0
+var closest_path: Path2D
+var closest_distance = 100000000000000
+
+func added_to_grid():
+	.added_to_grid()
+	
+	$Timer.start()
+	for path in get_tree().get_nodes_in_group("path"):
+		if(path.is_enemy == is_enemy):
+			var path_node: Path2D = path
+			var local_pos = path_node.to_local(global_position)
+			var distance = path_node.curve.get_closest_point(local_pos).distance_to(local_pos)
+			
+			if(distance < closest_distance):
+				closest_distance = distance
+				closest_path = path_node
+
+func _on_WarButton_pressed():
+	launch_war()
+
+func can_launch_war():
+	return len(dudes) <= 0 or count > 0
+	
+func launch_war():
+	count = 0
+	$WarTimer.start()
+	
+	_count_updated()
+	
+func set_invalid(value):
+	.set_invalid(value)
+	
+	$Label.visible = value
+var dudes = []
+var count = 0
+func _on_Timer_timeout():
+	if(get_dude_count() < max_dudes):
+		count += 1
+		var dude = load(unit_scene).instance()
+		dude.position = Vector2(randf(), randf()) * 10
+		dude.is_enemy = is_enemy
+		dudes.append(dude)
+		$DudeSpawnLocation.add_child(dude)
+		_count_updated()
+	
+func get_dude_count():
+	return len(dudes)
+func _count_updated():
+	return
+
+func _on_WarTimer_timeout():
+	var dude = dudes.pop_front()
+	if(not is_instance_valid(dude)):
+		return
+	dude.active = true
+	
+	if(len(dudes) <= 0):
+		$WarTimer.stop()
