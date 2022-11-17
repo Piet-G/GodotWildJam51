1fe98bc663652a6fd3ecb62e5744e6195ad6b743
stan depuydt <stanneke.depuydt@gmail.com>
2022-11-17 23:10:14 +0100

    heavy gun

---
+++ towers/archery_tower/HeavyMachineGunTower/HeavyMachineGunTower.gd
@@ -0,0 +1,24 @@
+extends "res://towers/archery_tower/AntiAirTower/AntiAirTower.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func shoot_at(dude: Node2D):
+	aim(dude)
+	
+	var arrow = preload("res://towers/archery_tower/HeavyMachineGunTower/HeavyArrow.tscn").instance()
+	arrow.is_enemy = is_enemy
+	arrow.origin = self
+
+	$ArrowPosition.add_child(arrow)
+	arrow.set_target(dude)
+	
+	$AnimatedSprite.play()
