1b1f84e28f83e4029b19a436e767d0172dc7bd68
stan depuydt <stanneke.depuydt@gmail.com>
2022-11-17 20:37:30 +0100

    anti air tower

--- towers/archery_tower/archery_tower.gd
+++ towers/archery_tower/archery_tower.gd
@@ -1,15 +1,17 @@
 extends "res://towers/tower.gd"
 
 export var damage = 1
+export var air = false
 
 func _on_ShootTimer_timeout():
 	if(not is_active()):
 		return
 		
 	for area in $Range.get_overlapping_areas():
-		if(area.is_in_group("dude_area") and not area.get_parent().targeted and area.get_parent().is_enemy != is_enemy and area.get_parent().is_active() and !area.get_parent().sneaking):
-			shoot_at(area.get_parent())
-			return
+		if((air && area.is_in_group("air")) || (!air && !area.is_in_group("air"))):
+			if(area.is_in_group("dude_area") and not area.get_parent().targeted and area.get_parent().is_enemy != is_enemy and area.get_parent().is_active() and !area.get_parent().sneaking):
+				shoot_at(area.get_parent())
+				return
 
 func shoot_at(dude: Node2D):
 	var arrow = preload("res://towers/archery_tower/arrow.tscn").instance()
