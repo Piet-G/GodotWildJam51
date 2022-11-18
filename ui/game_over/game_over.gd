909b2868fedf986568810f0a003434099b3cc42a
Piet Goris <piet.goris@gmail.com>
2022-11-18 12:37:00 +0100

    Better win/lose

---
+++ ui/game_over/game_over.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_MainMenu_pressed():
+	visible = false
+	get_tree().change_scene("res://ui/MainMenu.tscn")
+
+
+func _on_Button_pressed():
+	visible = false
+	get_tree().change_scene("res://ui/map.tscn")
