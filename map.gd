6b3fcfc116637374ef3935a87f247f85ff65cc5c
stan depuydt <stanneke.depuydt@gmail.com>
2022-11-18 13:45:23 +0100

    pausescreen

--- map.gd
+++ map.gd
@@ -9,7 +9,7 @@ func _init():
 	GridService.enemy_info = {}
 	GridService.illegal_spaces = {}
 	GridService.roads = {}
-	CastleService.hp = {true: 1, false: 100}
+	CastleService.hp = {true: 100, false: 100}
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
