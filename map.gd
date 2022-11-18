e8cb2826a1b91d1323bbc9c9a32427f77f3b14ab
stan depuydt <stanneke.depuydt@gmail.com>
2022-11-18 22:57:48 +0100

    added flashing castles

--- map.gd
+++ map.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var old_hp_blue = 100
+var old_hp_red = 100
+
 func _init():
 	ResourceManager.food = 0
 	ResourceManager.enemy_food = 0
@@ -14,5 +17,17 @@ func _init():
 # var a = 2
 # var b = "text"
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if CastleService.hp[false] < old_hp_blue:
+		$AnimationPlayer.play("BlueHit")
+	elif CastleService.hp[false] > old_hp_blue:
+		$AnimationPlayer.play("BlueRepair")
+	
+	old_hp_blue = CastleService.hp[false]
+	
+	if CastleService.hp[true] < old_hp_red:
+		$AnimationPlayer2.play("RedHit")
+	elif CastleService.hp[true] > old_hp_red:
+		$AnimationPlayer2.play("RedRepair")
+	
+	old_hp_red = CastleService.hp[true]
--- ui/castle_hp_bar/castle_hp_bar.gd
+++ ui/castle_hp_bar/castle_hp_bar.gd
@@ -4,3 +4,4 @@ export var is_enemy = false
 
 func _process(delta):
 	value = CastleService.get_hp_ratio(is_enemy)
+
