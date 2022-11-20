453de8dfbead4e56ebb104a73984d47d47d8a042
stan depuydt <stanneke.depuydt@gmail.com>
2022-11-20 01:54:28 +0100

    main menu

--- ui/MainMenu.gd
+++ ui/MainMenu.gd
@@ -12,5 +12,17 @@ func _ready():
 
 
 func _on_Start_Game_pressed():
+	$AudioStreamPlayer2.play()
+
+
+func _on_Timer_timeout():
+	$AnimatedSprite.play()
+
+
+func _on_AnimatedSprite_animation_finished():
+	$AnimatedSprite.playing = false
+
+
+func _on_AudioStreamPlayer2_finished():
 	Ui.visible = true
 	get_tree().change_scene("res://map.tscn")
